2782cfe6e3fb3c6c08bd97372dc455f58e746c91
valentinebudanov <budanov.vv@gmail.com>
2021-09-24 10:33:27 +0300

    Ex11: Тест запроса на метод cookie

--- my_test.py
+++ my_test.py
@@ -1,3 +1,13 @@
-def test_phrase_len():
-    phrase = input("Set a phrase: ")
-    assert len(phrase) <= 15, f"Phrase is more than 15 characters, it is {len(phrase)} characters long"
+import requests
+
+
+# def test_phrase_len():
+#     phrase = input("Set a phrase: ")
+#     assert len(phrase) <= 15, f"Phrase is more than 15 characters, it is {len(phrase)} characters long"
+
+
+def test_get_cookie():
+    response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
+    print('cookie>>>>>>  ' + str(response.cookies))
+    cookie = response.cookies.get('HomeWork')
+    assert cookie == 'hw_value'

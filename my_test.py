8094f7997df83af973ff4c352956eefc9b6c98cd
valentinebudanov <budanov.vv@gmail.com>
2021-09-24 10:45:30 +0300

    Ex12: Тест запроса на метод header

--- my_test.py
+++ my_test.py
@@ -9,5 +9,16 @@ import requests
 def test_get_cookie():
     response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
     print('cookie>>>>>>  ' + str(response.cookies))
-    cookie = response.cookies.get('HomeWork')
-    assert cookie == 'hw_value'
+    cookie_name = 'HomeWork'
+    assert cookie_name in response.cookies, f"There is no cookie {cookie_name} in response"
+    cookie_value = response.cookies.get(cookie_name)
+    assert cookie_value == 'hw_value', f"Cookie have wrong value - {cookie_value}"
+
+
+def test_get_header():
+    response = requests.get("https://playground.learnqa.ru/api/homework_header")
+    print('headers>>>>>>  ' + str(response.headers))
+    header_name = 'x-secret-homework-header'
+    assert header_name in response.headers, f"There is no header {header_name} in response"
+    header_value = response.headers.get(header_name)
+    assert header_value == 'Some secret value', f"Cookie have wrong value - {header_value}"

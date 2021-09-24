07a55f0e7b748b1c12a07101c98fb18e4770610d
valentinebudanov <budanov.vv@gmail.com>
2021-09-24 18:04:24 +0300

    Старт фреймворка

--- json_text.py
+++
@@ -1,8 +0,0 @@
-import json
-
-json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And ' \
-            'this is a second message","timestamp":"2021-06-04 16:41:01"}]} '
-
-parsed_json = json.loads(json_text)
-
-print(parsed_json["messages"][0]["message"])
---
+++ tests/test_user_auth.py
@@ -0,0 +1,44 @@
+import requests
+import pytest
+from lib.base_case import BaseCase
+
+
+class TestUserAuth(BaseCase):
+    exclude_params = [
+        ("no_cookie"),
+        ("no_header")
+    ]
+
+    def setup(self):
+        data = {
+            "email": "vinkotov@example.com",
+            "password": "1234"
+        }
+        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+
+        self.auth_sid = self.get_cookie(response1, "auth_sid")
+        self.token = self.get_header(response1, "x-csrf-token")
+        self.user_id_from_auth_method = self.get_json_value(response1, "user_id")
+
+    def test_user_auth(self):
+        response2 = requests.get(
+            "https://playground.learnqa.ru/api/user/auth",
+            headers={"x-csrf-token": self.token},
+            cookies={"auth_sid": self.auth_sid})
+        assert "user_id" in response2.json(), "There is no key 'user_id' in the response"
+        user_id_from_check_method = response2.json()["user_id"]
+        assert user_id_from_check_method == self.user_id_from_auth_method, "'user_id' doesn't compare"
+
+    @pytest.mark.parametrize("condition", exclude_params)
+    def test_negative_check(self, condition):
+        if condition == "no_cookie":
+            response2 = requests.get(
+                "https://playground.learnqa.ru/api/user/auth",
+                headers={"x-csrf-token": self.token})
+        else:
+            response2 = requests.get(
+                "https://playground.learnqa.ru/api/user/auth",
+                cookies={"auth_sid": self.auth_sid})
+        assert "user_id" in response2.json(), "There is no key 'user_id' in the second response"
+        user_id_from_check_method = response2.json()["user_id"]
+        assert user_id_from_check_method == 0, f"User is authorised by condition {condition}"
